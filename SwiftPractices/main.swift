a3b3668028afdc3809b48942e998a73c937cfa82
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2017-01-22 12:36:08 +0300

    First commit

---
+++ SwiftPractices/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPractices
+//
+//  Created by Samet ÇELİKBIÇAK on 22.01.2017.
+//  Copyright © 2017 Samet ÇELİKBIÇAK. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
