92892e663d5eae398ebf11f7b23c0ac73d70bc49
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2017-02-26 16:02:24 +0300

    text file read/write

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,7 @@
 //: Playground - noun: a place where people can play
 
 import Cocoa
+import Foundation
 
 var str = "Hello, playground"
 
@@ -33,3 +34,32 @@ func printSomethingReturnResult() -> String
 }
 var result2=printSomethingReturnResult()
 print(result2)
+
+
+
+let file = "file.txt" //this is the file. we will write to and read from it
+
+let text = "Some text, Hello Swift file read/write" //just a text
+
+var text2=""
+
+if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+    
+    let path = dir.appendingPathComponent(file)
+    
+    //writing
+    do {
+        try text.write(to: path, atomically: false, encoding: String.Encoding.utf8)
+    }
+    catch {/* error handling here */}
+    
+    //reading
+    do {
+        text2 = try String(contentsOf: path, encoding: String.Encoding.utf8)
+    }
+    catch {/* error handling here */}
+
+print(text2)
+    
+}
+
